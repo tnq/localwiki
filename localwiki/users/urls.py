a0e8fa8fa42702660a8e574cd5dfd95484eb3971
Quentin Smith <quentin@mit.edu>
2014-11-06 15:37:55 -0500

    Disable registration for localwiki (we don't use it anyway)

--- localwiki/users/urls.py
+++ localwiki/users/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import *
 from django.views.generic import TemplateView 
 
-from registration.views import register
-
 urlpatterns = patterns('',
-    url(r'^register/$', register,
-        {'backend': 'users.registration_backend.SaplingBackend'},
-        name='registration_register'),
+#    url(r'^register/$', RegistrationView.as_view,
+#        {'backend': 'users.registration_backend.SaplingBackend'},
+#        name='registration_register'),
     url(r'^register/closed/$', TemplateView.as_view(
             template_name='registration/registration_closed.html'),
         name='registration_disallowed'),
